1a25d202e062c229cfe18f160c529da04e75a9f2
Samuel Vaillant <samuel.vllnt@gmail.com>
2017-01-01 17:51:48 +0100

    feat: fetch info from github through micro client

--- src/Container.js
+++ src/Container.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
+import { query } from './client';
 
-/**
- * @name Container
- */
-export default class Container extends Component {
+class Container extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = { message: '<Container />' };
+    this.state = {
+      isLoading: true,
+      data: {},
+    };
+  }
+
+  componentDidMount() {
+    query(`
+      query($after: String) {
+        viewer {
+          login
+          repositories(
+            first: 5,
+            after: $after,
+            orderBy: {field: PUSHED_AT, direction: DESC},
+          ) {
+            edges {
+              node {
+                id,
+                name
+                description
+                createdAt
+                pushedAt
+              },
+              cursor,
+            }
+          }
+        }
+      }
+    `).then(res => {
+      this.setState({
+        isLoading: false,
+        data: res,
+      });
+    });
   }
 
   render() {
+    const { isLoading, data } = this.state;
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div className="container">
         <h1>React starter</h1>
-
-        Hello from {this.state.message} !!
+        Github account of <strong>{data.viewer.login}</strong>
+        <ul>
+          {data.viewer.repositories.edges.map(edge => (
+            <li key={edge.node.id}>{edge.node.name} - {edge.node.pushedAt}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 
 }
+
+export default Container;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureClient } from './client';
 import Container from './Container';
 import './index.css';
+
+configureClient('https://api.github.com/graphql', {
+  headers: {
+    Authorization: 'Bearer 1ea78121248da28183fe8044a4c887176ecf1301',
+  },
+});
 
 ReactDOM.render(
   <Container />,
