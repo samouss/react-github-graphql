7f70ea3101aa27354f91d9f012bda93262fed3ea
Samuel Vaillant <samuel.vllnt@gmail.com>
2017-02-16 11:05:47 +0100

    refactor: remove token and revoke it

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 configureClient('https://api.github.com/graphql', {
   headers: {
-    Authorization: 'Bearer 1ea78121248da28183fe8044a4c887176ecf1301',
+    Authorization: 'Bearer TOKEN_HERE',
   },
 });
 
